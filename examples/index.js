487eb0954a0f8559e5b94ef8c2d2608128c517aa
joehe <leanbrown@live.cn>
2020-02-03 22:23:47 +0800

    v1.0.6

--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,50 @@
 
 const {
-  bigimportProto,
   getClient
 } = require('../src/index.js')
-console.log(bigimportProto)
 
-const client = getClient('127.0.0.1:50051', '5e1c390fa625ed00194043ae')
-console.log(client)
+const {
+  updateDatasource,
+  findBySns,
+  upsertFile,
+  updateDatasourcevalue,
+  updateValue
+} = getClient('localhost:50051', '5e1c390fa625ed00194043ae')
+
+const main = async () => {
+  let res = await updateDatasource({
+    computeProcess: true,
+    cron: 'api',
+    name: 'VND26_kWh',
+    unit: 'kWh',
+    createdBy: '5cb9a68d4e01892bfe5ab855'
+  })
+  console.log(res)
+
+  res = await findBySns([1, 2])
+  console.log(res)
+
+  res = await upsertFile({
+    sns: 1,
+    relativePath: 'BURActiveEnergy-20140714-0200.csv',
+    mtime: new Date('Mon Jul 14 2014 02:00:02 GMT+0800 (GMT+08:00)')
+  })
+  console.log(res)
+
+  res = await updateDatasourcevalue({
+    name: 'F3_Normal_kWh',
+    time: new Date('2013-06-30T16:00:00.000Z'),
+    original: 347810,
+    // value: null,
+    upsert: true
+  })
+  console.log(res)
+
+  res = await updateValue({
+    name: 'VND26_kWh',
+    time: new Date('Mon Jul 14 2019 02:00:02 GMT+0800 (GMT+08:00)')
+  })
+  console.log(res)
+}
+
+main().then(() => console.log('---- end')).catch(err => console.error(err))
