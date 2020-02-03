b1dccd03f51ac8094ad667b8e93640e80ca346e7
joehe <leanbrown@live.cn>
2020-02-03 21:38:42 +0800

    v1.0.4

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,9 @@
 
-const bigimportProto = require('../src/index.js')
+const {
+  bigimportProto,
+  getClient
+} = require('../src/index.js')
 console.log(bigimportProto)
+
+const client = getClient('127.0.0.1:50051', '5e1c390fa625ed00194043ae')
+console.log(client)
